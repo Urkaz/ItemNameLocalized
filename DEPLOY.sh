d967cbdfdb268bb41b569ec289563b906b514664
Urkaz <mrurkaz@gmail.com>
2016-11-04 21:38:43 +0100

    Added bash script for easy deployment

---
+++ DEPLOY.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+ADDON_MAIN_FOLDER="ItemNameLocalized"
+ADDON_VERSION="v1.2"
+GAME_VERSION="7.1"
+
+FOLDERS=("AddonLocale" "ItemLocales")
+FILES=("Core.lua" "Preload.lua" "ItemNameLocalized.toc" "CHANGELOG.md" "README.md")
+
+echo "--------------------------"
+echo "> Deploying "$ADDON_MAIN_FOLDER
+echo "--------------------------"
+
+mkdir -p ./$ADDON_MAIN_FOLDER
+for i in "${FOLDERS[@]}"
+do
+	echo "> Copy folder: "$i
+	cp -r ./$i ./$ADDON_MAIN_FOLDER
+done
+echo "--------------------------"
+for i in "${FILES[@]}"
+do
+	echo "> Copy file: "$i
+	cp -r ./$i ./$ADDON_MAIN_FOLDER
+done
+
+echo "--------------------------"
+echo "> Deleting old zip"
+rm -f ./${ADDON_MAIN_FOLDER}_${ADDON_VERSION}_${GAME_VERSION}.zip
+echo "> Creating zip"
+zip -r ./${ADDON_MAIN_FOLDER}_${ADDON_VERSION}_${GAME_VERSION}.zip ./$ADDON_MAIN_FOLDER/*
+echo "> zip created"
+echo "--------------------------"
+rm -rf ./$ADDON_MAIN_FOLDER
+read -p "> Deploy finished. Press [Enter]..."
